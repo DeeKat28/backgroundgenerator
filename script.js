65b60fd4b3bc1c036e42f1f007b3f196a6b47af2
DeepakDevVerse <deepak.281295@gmail.com>
2020-10-16 23:46:04 +0530

    Fixed h3 content

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function setGradient() {
     + ", " 
     + color2.value 
     + ")";
-    css.textContent = body.style.background + ";";
+    css.textContent = body.style.background;
 }
 
 color1.addEventListener("input", setGradient);
